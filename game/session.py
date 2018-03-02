732db9220f459dc5dfea956699b1f56e207d8127
Martin Grogan <marting@lead-web.com>
2018-03-02 17:07:06 -0500

    removed the async feature on game logic

--- game/session.py
+++ game/session.py
@@ -1,5 +1,4 @@
 import numpy as np
-import asyncio
 import math
 
 from .grid import GameGrid
@@ -9,15 +8,11 @@ from .exceptions import ObstacleCollision
 DEFAULT_WIDTH = 10
 DEFAULT_HEIGHT = 10
 DEFAULT_SNAKE_WIDTH = 3
-DEFAULT_SPEED = 20
 
 
 class GameSession:
 
-    def __init__(self, width=DEFAULT_WIDTH,
-                 height=DEFAULT_HEIGHT,
-                 snake_width=DEFAULT_SNAKE_WIDTH,
-                 speed=DEFAULT_SPEED):
+    def __init__(self, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT, snake_width=DEFAULT_SNAKE_WIDTH):
         self.grid = GameGrid(width, height)
         self.grid.generate_snake(snake_width)
         self.direction = Direction.EAST
@@ -25,37 +20,34 @@ class GameSession:
         self.score = 0
         self.apple_base_point = math.ceil(width * height / 2)
         self.current_apple_point = self.apple_base_point
-        self.sleep_time = 1 / speed
-        self.is_running = False
 
     def matrix(self):
         return np.copy(self.grid.matrix)
 
     def change_direction(self, direction):
+        if direction is None:
+            return
         direction = Direction(direction)
         if direction % 2 != self.direction % 2:
             self.direction = direction
 
-    async def run(self):
+    def next(self):
         try:
-            self.is_running = True
-            while True:
-                cell = self.grid.move_snake(self.direction)
-                if cell == Cell.APPLE:
-                    self.score += self.current_apple_point
-                    self.current_apple_point = self.current_apple_point
-                    self.grid.generate_apple()
-                else:
-                    self.current_apple_point -= 1
-                    if self.current_apple_point <= 0:
-                        break
-                await asyncio.sleep(self.sleep_time)
+            cell = self.grid.move_snake(self.direction)
+            if cell == Cell.APPLE:
+                self.score += self.current_apple_point
+                self.current_apple_point = self.current_apple_point
+                self.grid.generate_apple()
+            else:
+                self.current_apple_point -= 1
+
+                # max number of moves to get apple has been reached
+                if self.current_apple_point <= 0:
+                    return False
+
         except ObstacleCollision:
-            pass
-        self.is_running = False
+            return False
+        return True
 
     def get_score(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.run())
-        loop.close()
         return self.score
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import asyncio
-
 from game.session import GameSession
 
 
 def main():
     session = GameSession()
+    i = 0
+    while session.next():
+        print(session.matrix())
+        print()
+        i += 1
+        if i & 5 == 0:
+            session.change_direction((i % 4) + 1)
+
     print(session.get_score())
 
 
